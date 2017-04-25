9c2d299bdf987988aabdbc2a6d481c6818bb3218
Patrick <mussi_kat@hotmail.com>
2017-04-25 12:02:53 +0200

    Projektv2

---
+++ src/museumvolenteer/DAL/ConnectionManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package museumvolenteer.DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * @author Nicolai, Emil, Patrick, Kasper, Casper
+ */
+public class ConnectionManager
+{
+    private static final String CONFIG_FILE_NAME = "VikingeMuseum.cfg";
+    private final SQLServerDataSource ds;
+    
+    public ConnectionManager() throws IOException
+    {
+        Properties props = new Properties();
+        props.load(new FileReader(CONFIG_FILE_NAME));
+        
+        ds = new SQLServerDataSource();
+        ds.setServerName(props.getProperty("SERVER"));
+        ds.setDatabaseName(props.getProperty("DATABASE"));
+        ds.setPortNumber(Integer.parseInt(props.getProperty("PORT")));
+        ds.setUser(props.getProperty("USER"));
+        ds.setPassword(props.getProperty("PASSWORD"));
+    }
+    
+    public Connection getConnection() throws SQLServerException
+    {
+        return ds.getConnection();
+    }
+    
+}
