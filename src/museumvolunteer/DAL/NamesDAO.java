75f3934ed856f4903df9a3c3ffee171d4c50fb41
casp122b@easv365.dk <ebn62qtd>
2017-05-17 21:26:30 +0200

    2 words solved sorting all lists and duplicate volunteers in same guild.

--- src/museumvolunteer/DAL/NamesDAO.java
+++ src/museumvolunteer/DAL/NamesDAO.java
@@ -160,7 +160,7 @@ public class NamesDAO {
     public List<Volunteer> getByGuildId(int guildsId) throws SQLException {
 
         List<Volunteer> allVolunteers = new ArrayList<>();
-        String sql = "SELECT * FROM Works_For INNER JOIN Names ON Works_For.nameId = Names.id WHERE Works_For.guildsId =  ?";
+        String sql = "SELECT DISTINCT * FROM Works_For INNER JOIN Names ON Works_For.nameId = Names.id WHERE Works_For.guildsId =  ? ORDER BY name ASC";
         try (Connection con = cm.getConnection()) {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, guildsId);
